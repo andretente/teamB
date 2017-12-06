6d4c08337e19385de27bad179cf22011d0af20b1
André Tente <iamandretente@gmail.com>
2017-12-06 22:38:50 +0100

    Lite update av hiscore och spel

--- www/js/classes/game.class.js
+++ www/js/classes/game.class.js
@@ -8,10 +8,10 @@ class SpelSetup extends Base{
     $('.form').append(
         `
         <div class="row mt-4">
-          <h3 class="col-12">Spelare ${player}</h3>
+          <h3 class="col-6 mx-auto">Spelare ${player}</h3>
         </div>
         <div class="row mt-2">
-          <div class="col-lg-6 d-inline-flex">
+          <div class="col-6 mx-auto d-inline-flex">
             <input type="text" class="form-control" id="playerName" aria-label="Spelare namn" placeholder="Spelare Namn">
             <button class="btn btn-secondary dropdown-toggle ml-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Type
@@ -35,7 +35,7 @@ class SpelSetup extends Base{
     $('.form').append(
       `
       <div class="row mt-4">
-        <div class="col-12">
+        <div class="col-6 mx-auto">
           <button type="button" class="btn btn-primary float-left" id="btn-addPlayers">Starta</button>
         </div>
       </div>
