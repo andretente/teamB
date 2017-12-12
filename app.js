f0e95b6b68b5c8cddd1aede5c8e9e67c4623bee9
Anjabing <anja.clundgren@gmail.com>
2017-12-12 14:50:25 +0100

    Lagt ett app class till

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use(flexjson);
 app.use(express.static('www'));
 
 // Start server
-app.listen(5000, () =>
-  console.log('Webserver listening on port 5000')
+app.listen(4000, () =>
+  console.log('Webserver listening on port 4000')
 );
