6cf75158d8becd7b03dd5d1372b952f01b090514
Unknown <tobbelobbe1997@live.se>
2017-12-18 10:03:59 +0100

    Renderar in innehåll från spelpage.class.js till spel.html

---
+++ www/js/classes/spelpage.class.js
@@ -0,0 +1,55 @@
+class SpelPage extends Base{
+
+	constructor(){
+		super();
+		this.renderSpelPage();
+	}
+
+	renderSpelPage(){
+		$('.spelpagecontent').append(`
+			<div>
+	          <div class="row mt-4">
+	            <h3 class="col-12 col-md-8 col-lg-8 mx-auto">Spelare 1</h3>
+	          </div>
+	          <div class="row mt-2">
+	            <div class="col-12 col-md-8 col-lg-8 mx-auto d-inline-flex">
+	              <div class="col-10 m-0 p-0">
+	                <input type="text" class="form-control" id="playerName1" aria-label="Spelare namn" placeholder="Spelare Namn">
+	              </div>
+	              <div class="col-2">
+	                <select class="form-control" id="type1">
+	                  <option>Human</option>
+	                  <option>Robot</option>
+	                </select>
+	              </div>
+	            </div>
+	          </div>
+	          
+	          <div class="row mt-4">
+	            <h3 class="col-12 col-md-8 col-lg-8 mx-auto">Spelare 2</h3>
+	          </div>
+
+	          <div class="row mt-2">
+	            <div class="col-12 col-md-8 col-lg-8 mx-auto d-inline-flex">
+	              <div class="col-10 m-0 p-0">
+	                <input type="text" class="form-control" id="playerName2" aria-label="Spelare namn" placeholder="Spelare Namn">
+	              </div>
+
+	              <div class="col-2">
+	                <select class="form-control" id="type2">
+	                  <option>Human</option>
+	                  <option>Robot</option>
+	                </select>
+	              </div>
+	            </div>
+	          </div>
+
+	          <div class="row mt-4">
+	            <div class="col-12 col-md-8 col-lg-8 mx-auto">
+	              <a href="board.html" class="btn btn-primary float-left" id="btn-addPlayers">Starta</a>
+	            </div>
+	          </div>
+			</div>
+		`);
+	}
+}
--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,11 @@
 let frontPage = new FrontPage();
 let rulesPage = new RulesPage();
+let spelPage = new SpelPage();
 let myBoard = new Board();
 let game = new Game();
 let myFooter = new Footer();
 
+spelPage;
 frontPage;
 rulesPage;
 myFooter;
