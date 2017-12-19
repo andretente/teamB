9237e9772cfec28e1912e689864fad68bf7f0754
Anjabing <anja.clundgren@gmail.com>
2017-12-19 14:25:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- www/js/main.js
+++ www/js/main.js
@@ -4,11 +4,13 @@ let spelPage = new SpelPage();
 let myBoard = new Board();
 let game = new Game();
 let myFooter = new Footer();
+let myWinpop = new Winpop();
 
 spelPage;
 frontPage;
 rulesPage;
 myFooter;
+myWinpop;
 
 JSON._load('players.json')
 .then(function(data){
