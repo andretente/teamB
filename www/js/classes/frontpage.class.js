a76527258f1835c05c86bdbc60e643452f16b2d9
Aiko M <minmilera@gmail.com>
2017-12-21 17:23:51 +0100

    uppdaterat lite css

--- www/js/classes/frontpage.class.js
+++ www/js/classes/frontpage.class.js
@@ -11,7 +11,7 @@ class FrontPage extends Base{
         <section class="container">
           <div class="row">
             <div class="col-sm-6">
-              <div class="card border-blue">
+              <div class="card border-blue mb-3 mt-4">
                 <div class="card-body">
 									<div class=row>
 										<div class="col-6">
@@ -27,7 +27,7 @@ class FrontPage extends Base{
               </div>
             </div>
             <div class="col-sm-6">
-							<div class="card border-blue">
+							<div class="card border-blue mt-4">
 								<div class="card-body">
 									<img class="w-100 img-fluid " src="/img/front.svg">
 								</div>
@@ -36,28 +36,28 @@ class FrontPage extends Base{
           </div>
           <div class="row mt-5">
 						<div class="card-deck">
-							<div class="card mb-3 border-blue">
+							<div class="card mb-5 border-blue">
 								<img class="card-img-top" src="/img/plants-3.jpg" alt="Card image cap">
 								<div class="card-body">
 									<h4 class="card-title">Plants and Zombies</h4>
 									<p class="card-text">Skydda ett hus från attackerande zombier genom att plantera flera olika plantor. </p>
-									<a href="#" class="btn btn-red float-right">Spela</a>
+									<a href="#" class="btn btn-red online-spel">Spela</a>
 								</div>
 							</div>
-							<div class="card mb-3 border-blue">
+							<div class="card mb-5 border-blue">
 								<img class="card-img-top" src="/img/pengu-3.jpg" alt="Card image cap">
 								<div class="card-body">
 									<h4 class="card-title">Pengu</h4>
 									<p class="card-text">Till skillnad från den klassiska Bomberman spelar man som namnet på spelet redan föreslår som en pingvin.</p>
-									<a href="#" class="btn btn-red float-right">Spela</a>
+									<a href="#" class="btn btn-red online-spel">Spela</a>
 								</div>
 							</div>
-							<div class="card mb-3 border-blue">
+							<div class="card mb-5 border-blue">
 								<img class="card-img-top" src="/img/heroes.jpg" alt="Card image cap">
 								<div class="card-body">
 									<h4 class="card-title">Heroes</h4>
 									<p class="card-text">Anslut dig till hjältarna i deras krig mot ondskan och skriv historia.</p>
-									<a href="#" class="btn btn-red float-right">Spela</a>
+									<a href="#" class="btn btn-red online-spel">Spela</a>
 								</div>
 							</div>
 						</div>
